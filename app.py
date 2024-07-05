c40a87da85ff4d8c50e60cd2bd0f76b2fc0c3080
Jack Killilea <jack.killilea@ericsson.com>
2024-07-05 14:56:00 +0100

    added booking creation functionality

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from sqlalchemy import select, func, text
+from sqlalchemy import select, func
 from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
 
 USERNAME = "root"
 PASSWORD = ""
@@ -26,6 +27,11 @@ class User(db.Model):
     last_name = db.Column(db.String(64), nullable=False)
     phone_number = db.Column(db.String(20), nullable=False)
 
+    def __init__(self, first_name, last_name, phone_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+
 
 class Reservation(db.Model):
     __tablename__ = "reservations"
@@ -34,6 +40,12 @@ class Reservation(db.Model):
     end_time = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
     table_id = db.Column(db.Integer, db.ForeignKey("tables.table_id"), nullable=False)
+
+    def __init__(self, start_time, end_time, user_id, table_id):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.user_id = user_id
+        self.table_id = table_id
 
 
 with app.app_context():
@@ -57,9 +69,34 @@ def get_bookings(table_id):
     reservations = [{"start_time": row[0], "end_time": row[1]}
                     for row in db.session.execute(statement)]
 
-    return list(reservations)
+    return reservations
 
 
-@app.route("/booking")
+@app.route("/booking", methods=["GET", "POST"])
 def booking():
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        phone_number = request.form.get("phone_number")
+        date = request.form.get("date")
+        time = request.form.get("time")
+        table_id = request.form.get("table_id")
+
+        if not all((first_name, last_name, phone_number, date, time, table_id)):
+            return render_template("booking.html")
+
+        # date format is "YYYY-MM-DD HH:MM"
+        start_time = datetime.strptime(f"{date} {time}",
+                                       "%Y-%m-%d %H:%M")
+        end_time = start_time + timedelta(hours=2)
+
+        user = User(first_name, last_name, phone_number)
+        db.session.add(user)
+        db.session.flush()
+
+        reservation = Reservation(start_time, end_time, user.user_id, table_id)
+        db.session.add(reservation)
+
+        db.session.commit()
+
     return render_template("booking.html")
