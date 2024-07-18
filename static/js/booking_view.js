d8875e9371d40e239bf0c240a56fa17d1f58f0f9
Jack Killilea <jack.killilea@ericsson.com>
2024-07-18 12:27:54 +0100

    added booking information popup to employee view page

---
+++ static/js/booking_view.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const booking_popup = document.getElementById("popup");
+    const show_popup = async (event, booking_id) => {
+        booking_popup.style.left = `${event.pageX}px`;
+        booking_popup.style.top = `${event.pageY}px`;
+
+        const res = await fetch(`/api/booking/${booking_id}`);
+        const reservation_info = await res.json();
+
+        console.log(reservation_info)
+        
+        document.getElementById("popup-reservation-id").innerHTML = booking_id;
+        document.getElementById("popup-time").innerHTML = reservation_info["start_time"] + " - " + reservation_info["end_time"];
+        document.getElementById("popup-name").innerHTML = reservation_info["name"];
+        document.getElementById("popup-phone-number").innerHTML = reservation_info["phone_number"];
+
+        booking_popup.hidden = false;
+    }
+
+    document.getElementById("content").addEventListener("click", () => {
+        booking_popup.hidden = true;
+    });
+
+    document.getElementById("popup-close-button").addEventListener("click", () => {
+        booking_popup.hidden = true;
+    });
+
+    // without this, clicking on the popup would make it disappear
+    document.getElementById("popup").addEventListener("click", (e) => {
+        e.stopPropagation();
+    });
+
+    const booking_squares = document.getElementsByClassName("booked")
+    Array.from(booking_squares).forEach((booking_square) => {
+        booking_square.addEventListener("click", (e) => {
+            e.stopPropagation()
+            show_popup(e, booking_square.dataset.id)
+        });
+    });
+});
