234a33601f1607fc6552a9a7a964d52f1d996d19
caoimhexnineachtain <caoimhe.naughton@ericsson.com>
2024-07-23 09:40:06 +0100

    delivery 2

--- app.py
+++ app.py
@@ -688,28 +688,9 @@ def giftcard():
 
     return render_template("giftcard.html")
 
-'''@app.route('/accountsettings', methods=["POST", "GET"])
-def accountdetails():
-    if request.method == 'POST':
-        new_email = request.form.get('email-change')
-        new_password = request.form.get('password-change')
-        confirm_new_password = request.form.get("confirm-password-change")
-        phone_no = request.form.get('phone-number-change')
-        user = db.session.execute(select(User).where(User.user_id == session.get("user_id"))).first()[0]
-        
-        
-        if confirm_new_password == new_password:
+@app.route('/delivery')
+def delivery():
 
-        elif phone_no == True:
-
-        elif new_email == True:
-
-
-        else:
-
-
-
-    return render_template("accountsettings.html")'''
-        
+    return render_template("delivery.html")
 
 
