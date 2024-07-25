647f662fc41d13332a019d98f2881b497a27e1af
Luke Quilty <Luke.quilty@ericsson.com>
2024-07-25 13:40:50 +0100

    app routes for delivery cart done

--- app.py
+++ app.py
@@ -894,7 +894,7 @@ def delivery():
         name = request.form['name']
         address = request.form['address']
         phone_number = request.form['phone_number']
-        order_details = request.form['order_details']
+        order_details = request.form['order-details']
         specifications = request.form['specifications']
 
         delivery = Order(name, address, phone_number, order_details, specifications)
